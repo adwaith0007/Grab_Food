ae3704a3c6c78a7f6f7a5f6a538b4935ce0ca14f
Adwaith.k <102245239+adwaith0007@users.noreply.github.com>
2023-12-19 23:48:51 +0530

    backend jwt w

--- server/Controllers/userControllers.js
+++ server/Controllers/userControllers.js
@@ -1,9 +1,22 @@
 const UserModel = require("../Models/userModels");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const {body,validationResult} = require('express-validator');
+
+
+
+
+const maxAge = 3 * 24 * 60 * 60 ;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secretkey',{
+        expiresIn: maxAge
+    })
+}
 
 exports.signUp = async (req, res) => {
   const { fname, lname, email, password, phone } = req.body;
+
+
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -30,25 +43,66 @@ exports.signUp = async (req, res) => {
 
 
 
+// exports.verifyLogin_post = async (req, res) => {
+//   const { email, password } = req.body;
+
+  
+// try{
+
+
+//   const userData = await UserModel.findOne({email:email});
+
+//   if(userData){
+
+//     const passwordMatch = await bcrypt.compare(password,userData.hashedPassword);
+
+//     if(passwordMatch){
+//       const token = createToken(userData._id);
+//         res.cookie('jwt', token,{ httpOnly: true, maxAge: maxAge * 1000 });
+//            res.json({ success: true, message: 'Login successful' });
+//         console.log('good');
+//     }else{
+//         console.log('sorry');
+//         res.status(401).json({ success: false, message: 'Invalid credentials' });
+//     }
+
+//   }else{
+//     res.status(404).json({ success: false, message: 'User not found' });
+//     console.log('somthing went wrong');
+//   }
+
+// } catch (error) {
+//         console.error("Error during login:", error.message);
+//         res.status(500).json({ success: false, message: "Internal Server Error" });
+//     }
+
+  
+// };
+
+
 exports.verifyLogin_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(`email: ${email}`);
+  try {
+      const userData = await UserModel.findOne({ email: email });
 
-  const userData = await UserModel.findOne({email:email});
+      if (userData) {
+          const passwordMatch = await bcrypt.compare(password, userData.hashedPassword);
 
-  if(userData){
-
-    const passwordMatch = await bcrypt.compare(password,userData.password);
-
-
-    if(passwordMatch){
-        console.log('good');
-    }else{
-        console.log('sorry');
-    }
-
+          if (passwordMatch) {
+              const token = createToken(userData._id);
+              res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000, path: '/', domain: 'yourdomain.com' });
+              res.json({ success: true, message: 'Login successful' });
+              console.log('good');
+          } else {
+              res.status(401).json({ success: false, message: 'Invalid credentials' });
+              console.log('not good');
+          }
+      } else {
+          res.status(404).json({ success: false, message: 'User not found' });
+      }
+  } catch (error) {
+      console.error("Error during login:", error.message);
+      res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-
-  
 };
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -2,12 +2,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/userControllers");
+// const { check, validationResult } = require('express-validator');
 
 
 
-
-router.post('/register',(req,res)=>{
+router.post('/register', 
+// [body('email').isEmail()],
+(req,res)=>{
     userController.signUp(req, res);
+   
+    // res.send("Hello World")
+    // const errors= validationResult(req);
+    // if(!errors.isEmpty()){
+    //     return res.status(400).json({errors:errors.array()})
+    // }
 })
 
 router.post('/login',(req,res)=>{
